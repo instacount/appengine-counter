8758b637b6485ae3fb53348b2e649da091ded321
David Fuelling <sappenin@gmail.com>
2014-12-08 17:38:30 -0700

    Initial Commit of test class

---
+++ src/test/java/com/theupswell/appengine/counter/service/OpsPerSecondCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.theupswell.appengine.counter.service;
+
+import junit.framework.TestCase;
+
+public class OpsPerSecondCalculatorTest extends TestCase
+{
+
+	public void testGetOpsPerSecond() throws Exception
+	{
+
+	}
+
+	public void testGetNumShards() throws Exception
+	{
+
+	}
+}
