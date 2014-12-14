24b6fa34b79a333b999fb5f62de8801a89bd6176
David Fuelling <sappenin@gmail.com>
2014-12-13 23:28:24 -0700

    Fix unit test

--- src/test/java/com/theupswell/appengine/counter/service/ShardedCounterServiceIncrementTest.java
+++ src/test/java/com/theupswell/appengine/counter/service/ShardedCounterServiceIncrementTest.java
@@ -1,17 +1,14 @@
 /**
  * Copyright (C) 2014 UpSwell LLC (developers@theupswell.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.theupswell.appengine.counter.service;
 
@@ -26,8 +23,7 @@ import com.theupswell.appengine.counter.data.CounterData;
 import com.theupswell.appengine.counter.data.CounterData.CounterStatus;
 
 /**
- * Unit tests for incrementing a counter via
- * {@link com.theupswell.appengine.counter.service.ShardedCounterServiceImpl}.
+ * Unit tests for incrementing a counter via {@link com.theupswell.appengine.counter.service.ShardedCounterServiceImpl}.
  * 
  * @author David Fuelling
  */
@@ -66,7 +62,8 @@ public class ShardedCounterServiceIncrementTest extends
 		}
 		catch (RuntimeException e)
 		{
-			assertEquals("Can't increment counter \"" + TEST_COUNTER1 + "\" because it is currently being deleted!",
+			assertEquals("Can't mutate the amount of counter \"" + TEST_COUNTER1
+				+ "\" because it's currently in the DELETING state but must be in in the AVAILABLE state!",
 				e.getMessage());
 			throw e;
 		}
